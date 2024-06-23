60fb4d8fe8a9246ea8a0df0d98058e1d083be47b
Burhan <burhanux@gmail.com>
2024-06-23 16:47:06 -0700

    UI updates for CollapseUIEL

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
+const SampleData = [
+  { name: "Tab1", url: "https://google.com", timestamp: new Date().toLocaleString() },
+  { name: "Tab2", url: "https://google.com", timestamp: new Date().toLocaleString() },
+  { name: "Tab3", url: "https://google.com", timestamp: new Date().toLocaleString() },
+  { name: "Tab4", url: "https://google.com", timestamp: new Date().toLocaleString() },
+]
+
 function App() {
   return (
     <>
@@ -19,6 +26,8 @@ const CollapseUI = (props: ICollapseUI) => {
   const [modeSymbol, setModeSymbol] = useState<boolean>(false)
   const refBtn = useRef()
   const refContent = useRef()
+  const TAB_COLOR = "rgba(51, 170, 51, 0.8)";
+  const EL_BG_COLOR = "rgba(51, 170, 51, 0.2)"
 
   useEffect(() => {
     window.onload = (event) => {
@@ -29,7 +38,6 @@ const CollapseUI = (props: ICollapseUI) => {
 
     function handleClick(e) {
       this.classList.toggle("active");
-      // this.classList.add("active");
       var content = this.nextElementSibling;
       if (!this.classList.contains("active") && content.style.maxHeight) {
         content.style.maxHeight = null;
@@ -44,9 +52,30 @@ const CollapseUI = (props: ICollapseUI) => {
   }, [refBtn])
 
   return (<>
-    <button ref={refBtn} type="button" className="collapsible" style={{ display: "flex", justifyContent: "space-between", fontWeight: "bold" }}><span>{props.TAB_TITLE}</span> <span><strong>{modeSymbol ? "-" : "+"}</strong></span></button>
+    <button ref={refBtn} type="button" className="collapsible" style={{ display: "flex", justifyContent: "space-between", fontWeight: "bold", }}><span>{props.TAB_TITLE}</span> <span><strong>{modeSymbol ? "-" : "+"}</strong></span></button>
     <div ref={refContent} className="content">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id culpa tempora est excepturi hic suscipit non odio cum eveniet nostrum! Veniam odio excepturi corrupti eum pariatur cumque eius, unde alias.</p>
+      {SampleData.map((data: { name: string, url: string, timestamp: string }, index: number) => <CollapseUIEl key={index} name={data.name} url={data.url} date={data.timestamp} RGBABorder={TAB_COLOR} RGBAContainer={EL_BG_COLOR} />)}
     </div>
   </>)
+}
+
+interface ICollapseUIEl {
+  name: string,
+  date: string,
+  url: string
+  RGBABorder?: string,
+  RGBAContainer?: string,
+}
+
+const CollapseUIEl = (props: ICollapseUIEl) => {
+  const borderColor = props.RGBABorder ? props.RGBABorder : "inherit";
+  const bgColor = props.RGBAContainer ? props.RGBAContainer : "inherit";
+  const ElColor = { borderColor: borderColor, backgroundColor: bgColor }
+  return (
+    <div style={{ display: "flex", padding: "0.5rem", justifyContent: "space-between", border: "2px solid", borderRadius: "5px", marginTop: "0.5rem", ...ElColor }}>
+      <div>{props.name}</div>
+      <div>{props.date}</div>
+      <div>{props.url}</div>
+    </div>
+  )
 }
