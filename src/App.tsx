e4736bd70eae20ac0b2951c854e894acb66ce4bc
Burhan <burhanux@gmail.com>
2024-06-23 14:04:39 -0700

    Add modeSymbolf for tab

--- src/App.tsx
+++ src/App.tsx
@@ -2,53 +2,25 @@ import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
-  useEffect(() => {
-    // var coll: any = document.getElementsByClassName("collapsible");
-    // var i;
-    // console.log("coll", coll)
-    // for (i = 0; i < coll.length; i++) {
-    //   coll[i].addEventListener("click", handleClick, false)
-    // }
-
-    // console.log(`Counter `, i)
-
-    // function handleClick(e) {
-
-    //   console.log("e", e)
-    //   console.log("this", this)
-    //   console.log('this.classList.contains("active)"', this.classList.contains("active"))
-    //   // this.classList.toggle("active");
-    //   this.classList.add("active");
-    //   var content = this.nextElementSibling;
-    //   if (!this.classList.contains("active") && content.style.maxHeight) {
-    //     console.log("1 content.style.maxHeight", content.style.maxHeight)
-    //     content.style.maxHeight = null;
-    //   } else if (this.classList.contains("active")) {
-    //     console.log("2 content.scrollHeight", content.scrollHeight)
-    //     console.log("2 content.style.maxHeight", content.style.maxHeight)
-    //     content.style.maxHeight = content.scrollHeight + "px";
-    //   }
-    //   e.stopPropagation();
-    //   e.preventDefault();
-    // }
-  }, [])
   return (
     <>
-      <CollapseUI />
+      <CollapseUI TAB_TITLE={"Tab 1"} />
     </>
   )
 }
 
 export default App
 
+interface ICollapseUI {
+  TAB_TITLE: string,
+}
 
-const CollapseUI = () => {
+const CollapseUI = (props: ICollapseUI) => {
+  const [modeSymbol, setModeSymbol] = useState<boolean>(false)
   const refBtn = useRef()
   const refContent = useRef()
 
   useEffect(() => {
-    console.log("refBtn", refBtn)
-    console.log("refContent", refContent)
     window.onload = (event) => {
       if (refBtn && refBtn.current) {
         refBtn.current.addEventListener("click", handleClick, false)
@@ -56,20 +28,15 @@ const CollapseUI = () => {
     };
 
     function handleClick(e) {
-      console.log("e", e)
-      console.log("this", this)
-      console.log('this.classList.contains("active)"', this.classList.contains("active"))
       this.classList.toggle("active");
       // this.classList.add("active");
       var content = this.nextElementSibling;
       if (!this.classList.contains("active") && content.style.maxHeight) {
-        console.log("1 content.style.maxHeight", content.style.maxHeight)
         content.style.maxHeight = null;
       } else if (this.classList.contains("active")) {
-        console.log("2 content.scrollHeight", content.scrollHeight)
-        console.log("2 content.style.maxHeight", content.style.maxHeight)
         content.style.maxHeight = content.scrollHeight + "px";
       }
+      setModeSymbol(prev => !prev)
       e.stopPropagation();
       e.preventDefault();
     }
@@ -77,7 +44,7 @@ const CollapseUI = () => {
   }, [refBtn])
 
   return (<>
-    <button ref={refBtn} type="button" className="collapsible">Open Collapsible</button>
+    <button ref={refBtn} type="button" className="collapsible" style={{ display: "flex", justifyContent: "space-between", fontWeight: "bold" }}><span>{props.TAB_TITLE}</span> <span><strong>{modeSymbol ? "-" : "+"}</strong></span></button>
     <div ref={refContent} className="content">
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id culpa tempora est excepturi hic suscipit non odio cum eveniet nostrum! Veniam odio excepturi corrupti eum pariatur cumque eius, unde alias.</p>
     </div>
